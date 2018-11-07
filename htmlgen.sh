e2ad24040c19dec06c860cbbc37bc3a08fe953b5
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2018-11-07 13:49:16 -0600

    A simple script for generating html skeletons.

---
+++ htmlgen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# htmlgen.sh
+# - creates an empty html file
+
+touch "$1"
+echo "<!DOCTYPE html>\n"\
+"<html>\n"\
+"\t<head>\n"\
+"\t\t<meta charset=\"UTF-8\">\n"\
+"\t\t<meta name=\"viewport\"\n"\
+"\t\tcontent=\"width=divice-width, user-scalable=no, initial-scale=1.0\">\n"\
+"\t\t<meta http-equiv=\"X-UA-Compatable\" content=\"ie=edge\">\n"\
+"\t\t<style></style>\n"\
+"\t</head>\n"\
+"\t<body>\n"\
+"\t\t<header>\n"\
+"\t\t</header>\n"\
+"\t\t<main>\n"\
+"\t\t</main>\n"\
+"\t\t<footer>\n"\
+"\t\t</footer>\n"\
+"\t</body>\n"\
+"</html>" > "$1"
