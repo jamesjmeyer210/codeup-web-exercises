294a02f4fe971430a6cac26fb8b054c6f88df7df
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2018-12-18 16:22:40 -0600

    added fetch and it works natively

--- js/promises.js
+++ js/promises.js
@@ -1,20 +1,29 @@
-const wait = (time) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve();
-        }, time);
+"use strict";
+const fetch = require("node-fetch");
+// 
+// const wait = (time) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           resolve();
+//         }, time);
+//     });
+// }
+//
+// wait(1000).then(() => console.log("You'll see this after 1 second"));
+// wait(3000).then(() => console.log("You'll see this after 3 seconds"));
+
+const githubUserStatus = (token) => {
+  const url = "https://api.github.com/users/:jamesjmeyer210/events"
+  return fetch(url, {headers: {'Authorization': `${token}`}})
+    .then( (response) => {
+      console.log(response.json());
+    })
+    .catch(error => {
+      console.error(error);
     });
 }
 
-wait(1000).then(() => console.log("You'll see this after 1 second"));
-wait(3000).then(() => console.log("You'll see this after 3 seconds"));
+const token = "d303b10843bbbb7ed87d16900cd8ac7f73c98b84e";
+githubUserStatus(token);
 
-// const myPromise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         if (Math.random() > 0.5) {
-//             resolve();
-//         } else {
-//             reject();
-//         }
-//     }, 1500);
-// });
+//fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
