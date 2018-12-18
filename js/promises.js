cfe07a1503a71b6ac3562a1105cb3d3af3ef9f7b
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2018-12-18 15:50:44 -0600

    finished first part of promises

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,20 @@
+const wait = (time) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, time);
+    });
+}
+
 wait(1000).then(() => console.log("You'll see this after 1 second"));
 wait(3000).then(() => console.log("You'll see this after 3 seconds"));
+
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (Math.random() > 0.5) {
+//             resolve();
+//         } else {
+//             reject();
+//         }
+//     }, 1500);
+// });
